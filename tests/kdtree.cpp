330a017afccdfb272932f5b90efe8fe993854832
Sergio <sero7360@colorado.com>
2024-12-08 01:16:09 +0000

    finishing insert function and helper function

--- tests/kdtree.cpp
+++ tests/kdtree.cpp
@@ -27,7 +27,7 @@ public:
     ~kdtree(){};
     node *init_node(float x_val, float y_val);
     void insert(node *new_node);
-    bool LeftRight(float node_val, float compare_val); // helper function
+    node *leftOrRight(node *current, node *new_node, int level); //helper function
 };
 
 kdtree::kdtree()
@@ -47,9 +47,45 @@ node *kdtree::init_node(float x_val, float y_val)
     return new_node;
 }
 
-bool kdtree::LeftRight(float node_val, float compare_val)
+node *kdtree::leftOrRight(node *current, node *new_node, int level)
 {
-    return node_val < compare_val ? true : false;
+    float current_val = (level % K == 1) ? current->x : current->y;
+    float new_val = (level % K == 1) ? new_node->x : new_node->y;
+
+    if (current_val < new_val)
+    {
+        //! current value is LARGER goes RIGHT
+        if (current->right == nullptr)
+        {
+            current->right = new_node;
+            cout << "current right is node the new node :" << new_node->x << endl;
+            current = nullptr;
+        }
+        else
+        {
+            current == current->right;
+        }
+    }
+    else
+    {
+        //current value is SMALLER goes LEFT
+        if (current->left == nullptr)
+        {
+            current->left = new_node;
+            cout << "current left is node the new node :" << new_node->x << endl;
+            current = nullptr;
+        }
+        else
+        {
+            current == current->left;
+        }
+    }
+
+
+    // cout << "value depending on level for current: " << current_val << endl;
+    // cout << "value depending on level for new node: " << new_val << endl;
+
+    return current;
 }
 
 void kdtree::insert(node *new_node)
@@ -57,27 +93,53 @@ void kdtree::insert(node *new_node)
     if (*root == nullptr)
     {
         *root = new_node;
+        cout << "new node is new root:" << new_node->x << endl;
     }
     else
     {
         node *current = *root;
-        int level = 0; // inital k level is meant for the x axis equals 1
+        int level = 1; // inital k level is meant for the x axis equals 1
 
         while (current != nullptr)
         {
-            level +=1;
-
-            if (LeftRight( current->x , new_node->x))
+            if (level % K == 1)
             {
-                if (current->left)
+                if (current->x < new_node->x)
                 {
-                    /* code */
+                    //! current value is LARGER goes RIGHT
+                    if (current->right == nullptr)
+                    {
+                        current->right = new_node;
+                        cout << "current right is node the new node :" << new_node->x << endl;
+                        current = nullptr;
+                    }
+                    else
+                    {
+                        current == current->right;
+                        level += 1;
+                    }
                 }
-                
+                else
+                {
+                    //current value is SMALLER goes LEFT
+                    if (current->left == nullptr)
+                    {
+                        current->left = new_node;
+                        cout << "current left is node the new node :" << new_node->x << endl;
+                        current = nullptr;
+                    }
+                    else
+                    {
+                        current == current->left;
+                        level += 1;
+                    }
+                }
             }
-            
-
-
+            else
+            {
+                cout << "other" << endl;
+                current = nullptr;
+            }
         }
     }
 }
@@ -88,8 +150,19 @@ int main()
     vector<float> yCSV = {-787, 50, -311, -280.5, -532.5, -680.5, -320.5, -720.5};
 
     kdtree mykdtree;
-    cout << mykdtree.LeftRight(2587, 2603) << endl;
-    cout << "ALL GOOOOOOD" << endl;
+    node *p1 = mykdtree.init_node(2587, -787);
+    node *p2 = mykdtree.init_node(240.5, 50);
+    node *p3 = mykdtree.init_node(2603.5, -311);
+    node *p4 = mykdtree.init_node(2814.5, 90);
+
+    // mykdtree.insert(p1);
+    // mykdtree.insert(p2);
+    // mykdtree.insert(p3);
+    // mykdtree.insert(p4);
+
+    mykdtree.leftOrRight(p3, p4, 2);
+
+    // cout << "ALL GOOOOOOD" << endl;
 
     return 0;
 }
